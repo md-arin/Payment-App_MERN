0f602077552a8a6a1c7f36568f931639193d10f1
Md Arin Alam <alamlew@gmail.com>
2024-01-25 22:10:15 +0530

    implemented get route to show users filterable via first name/lastname

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -162,4 +162,29 @@ router.put("/", authmiddleware, async(req,res)=>{
    
 })
 
+router.get("/bulk", async(req,res)=>{
+    const filter = req.query.filter || "";
+
+    const users = await User.find({
+        $or: [{
+            firstName: {
+                "$regex": filter
+            }
+        }, {
+            lastName: {
+                "$regex": filter
+            }
+        }]
+    })
+
+    res.json({
+        user: users.map(user=>({
+            username: user.username,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            _id: user._id
+        }))
+    })
+})
+
 module.exports = router
